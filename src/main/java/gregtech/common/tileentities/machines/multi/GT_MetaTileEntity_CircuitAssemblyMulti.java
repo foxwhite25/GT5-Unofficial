a5521fdf8e94b289ba1f9c54d8ef66e629d66eae
TheEpicGamer274 <102255081+TheEpicGamer274@users.noreply.github.com>
2023-07-25 01:09:24 -0400

    Savin

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_CircuitAssemblyMulti.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_CircuitAssemblyMulti.java
@@ -104,18 +104,25 @@ public class GT_MetaTileEntity_CircuitAssemblyMulti
     }
 
     @Override
-    public void construct(ItemStack stackSize, boolean hintsOnly) {
-
-    }
-
-    @Override
     public IStructureDefinition<GT_MetaTileEntity_CircuitAssemblyMulti> getStructureDefinition() {
         return null;
     }
 
     @Override
     protected GT_Multiblock_Tooltip_Builder createTooltip() {
-        return null;
+        final GT_Multiblock_Tooltip_Builder tt = new GT_Multiblock_Tooltip_Builder();
+        tt.addMachineType("Circuit Assembler")
+            .addInfo("Assisting in all your Circuit needs.")
+            .addInfo("All EU is deducted from wireless EU networks only.")
+            .addInfo("AuthorTheEpicGamer274")
+            .addSeparator()
+            .beginStructureBlock(5, 7, 5, false)
+            .addStructureInfo(GOLD + "1+ " + GRAY + "Input Hatch")
+            .addStructureInfo(GOLD + "1+ " + GRAY + "Output Hatch")
+            .addStructureInfo(GOLD + "1+ " + GRAY + "Input Bus")
+            .addStructureInfo(GOLD + "1 " + GRAY + "Maintenance Hatch")
+            .toolTipFinisher("Gregtech");
+        return tt;
     }
 
     @Override
@@ -123,147 +130,92 @@ public class GT_MetaTileEntity_CircuitAssemblyMulti
         return GT_Recipe.GT_Recipe_Map.sNanoForge;
     }
 
+    private String ownerUUID;
+    int multiplier = 1;
+    long mWirelessEUt = 0;
+
+    @Override
+    protected ProcessingLogic createProcessingLogic() {
+        return new ProcessingLogic() {
+
+            @NotNull
+            @Override
+            protected CheckRecipeResult validateRecipe(@Nonnull GT_Recipe recipe) {
+                mWirelessEUt = 10L * (long) recipe.mEUt * (long) multiplier;
+                if (!addEUToGlobalEnergyMap(ownerUUID, -mWirelessEUt * recipe.mDuration)) {
+                    return CheckRecipeResultRegistry.insufficientPower(mWirelessEUt * recipe.mDuration);
+                }
+                return CheckRecipeResultRegistry.SUCCESSFUL;
+            }
+
+            @Nonnull
+            @Override
+            protected GT_OverclockCalculator createOverclockCalculator(@Nonnull GT_Recipe recipe,
+                                                                       @Nonnull GT_ParallelHelper helper) {
+                return GT_OverclockCalculator.ofNoOverclock(recipe);
+            }
+
+            @NotNull
+            @Override
+            public CheckRecipeResult process() {
+                CheckRecipeResult result = super.process();
+                // Power will be directly consumed through wireless
+                setCalculatedEut(0);
+                return result;
+            }
+        }.setMaxParallelSupplier(() -> {
+            ItemStack controllerStack = getControllerSlot();
+            if (controllerStack != null && controllerStack.getItem() instanceof GT_IntegratedCircuit_Item) {
+                multiplier = controllerStack.stackSize * max(1, controllerStack.getItemDamage());
+            }
+            return multiplier;
+        });
+    }
+
+    @Override
+    protected void setProcessingLogicPower(ProcessingLogic logic) {
+        // The voltage is only used for recipe finding
+        logic.setAvailableVoltage(Long.MAX_VALUE);
+        logic.setAvailableAmperage(1);
+    }
+
     @Override
     public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
-        return new GT_MetaTileEntity_CircuitAssemblyMulti(this.mName) {
+        return new GT_MetaTileEntity_CircuitAssemblyMulti(mName);
+    }
 
-            @Override
-            public IStructureDefinition<GT_MetaTileEntity_CircuitAssemblyMulti> getStructureDefinition() {
-                return STRUCTURE_DEFINITION;
-            }
+    @Override
+    public void saveNBTData(NBTTagCompound aNBT) {
+        aNBT.setInteger("eMultiplier", multiplier);
+        super.saveNBTData(aNBT);
+    }
 
-            @Override
-            protected GT_Multiblock_Tooltip_Builder createTooltip() {
-                final GT_Multiblock_Tooltip_Builder tt = new GT_Multiblock_Tooltip_Builder();
-                tt.addMachineType("Circuit Assembler")
-                    .addInfo("Assisting in all your Circuit needs.")
-                    .addInfo("All EU is deducted from wireless EU networks only.")
-                    .addInfo("AuthorTheEpicGamer274")
-                    .addSeparator()
-                    .beginStructureBlock(5, 7, 5, false)
-                    .addStructureInfo(GOLD + "1+ " + GRAY + "Input Hatch")
-                    .addStructureInfo(GOLD + "1+ " + GRAY + "Output Hatch")
-                    .addStructureInfo(GOLD + "1+ " + GRAY + "Input Bus")
-                    .addStructureInfo(GOLD + "1 " + GRAY + "Maintenance Hatch")
-                    .toolTipFinisher("Gregtech");
-                return new GT_Multiblock_Tooltip_Builder();
-            }
+    @Override
+    public void loadNBTData(final NBTTagCompound aNBT) {
+        multiplier = aNBT.getInteger("eMultiplier");
+        super.loadNBTData(aNBT);
+    }
 
-            private String ownerUUID;
-            int multiplier = 1;
-            long mWirelessEUt = 0;
+    @Override
+    public void construct(ItemStack stackSize, boolean hintsOnly) {
+        buildPiece(STRUCTURE_PIECE_MAIN, stackSize, hintsOnly, 0, 1, 0);
+    }
+    private static final int HORIZONTAL_OFFSET = 2;
+    private static final int VERTICAL_OFFSET = 3;
+    private static final int DEPTH_OFFSET = 0;
 
-            @Override
-            protected ProcessingLogic createProcessingLogic() {
-                return new ProcessingLogic() {
-
-                    @NotNull
-                    @Override
-                    protected CheckRecipeResult validateRecipe(@Nonnull GT_Recipe recipe) {
-                        mWirelessEUt = 10L * (long) recipe.mEUt * (long) multiplier;
-                        if (!addEUToGlobalEnergyMap(ownerUUID, -mWirelessEUt * recipe.mDuration)) {
-                            return CheckRecipeResultRegistry.insufficientPower(mWirelessEUt * recipe.mDuration);
-                        }
-                        return CheckRecipeResultRegistry.SUCCESSFUL;
-                    }
-
-                    @Nonnull
-                    @Override
-                    protected GT_OverclockCalculator createOverclockCalculator(@Nonnull GT_Recipe recipe,
-                        @Nonnull GT_ParallelHelper helper) {
-                        return GT_OverclockCalculator.ofNoOverclock(recipe);
-                    }
-
-                    @NotNull
-                    @Override
-                    public CheckRecipeResult process() {
-                        CheckRecipeResult result = super.process();
-                        // Power will be directly consumed through wireless
-                        setCalculatedEut(0);
-                        return result;
-                    }
-                }.setMaxParallelSupplier(() -> {
-                    ItemStack controllerStack = getControllerSlot();
-                    if (controllerStack != null && controllerStack.getItem() instanceof GT_IntegratedCircuit_Item) {
-                        multiplier = controllerStack.stackSize * max(1, controllerStack.getItemDamage());
-                    }
-                    return multiplier;
-                });
-            }
-
-            @Override
-            protected void setProcessingLogicPower(ProcessingLogic logic) {
-                // The voltage is only used for recipe finding
-                logic.setAvailableVoltage(Long.MAX_VALUE);
-                logic.setAvailableAmperage(1);
-            }
-            @Override
-            public void saveNBTData(NBTTagCompound aNBT) {
-                aNBT.setInteger("eMultiplier", multiplier);
-                super.saveNBTData(aNBT);
-            }
-
-            @Override
-            public void loadNBTData(final NBTTagCompound aNBT) {
-                multiplier = aNBT.getInteger("eMultiplier");
-                super.loadNBTData(aNBT);
-            }
-
-            @Override
-            public void construct(ItemStack stackSize, boolean hintsOnly) {
-                buildPiece(STRUCTURE_PIECE_MAIN, stackSize, hintsOnly, 0, 1, 0);
-            }
-            private static final int HORIZONTAL_OFFSET = 2;
-            private static final int VERTICAL_OFFSET = 3;
-            private static final int DEPTH_OFFSET = 0;
-
-            @Override
-            public int survivalConstruct(ItemStack stackSize, int elementBudget, ISurvivalBuildEnvironment env) {
-                return survivialBuildPiece(
-                    STRUCTURE_PIECE_MAIN,
-                    stackSize,
-                    HORIZONTAL_OFFSET,
-                    VERTICAL_OFFSET,
-                    DEPTH_OFFSET,
-                    elementBudget,
-                    env,
-                    false,
-                    true);
-            }
-
-            @Override
-            public boolean isCorrectMachinePart(ItemStack aStack) {
-                return false;
-            }
-
-            @Override
-            public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
-                if (!checkExoticAndNormalEnergyHatches()) {
-                }
-                return false;
-            }
-
-            @Override
-            public int getMaxEfficiency(ItemStack aStack) {
-                return 10000;
-            }
-
-            @Override
-            public int getDamageToComponent(ItemStack aStack) {
-                return 0;
-            }
-
-            @Override
-            public boolean explodesOnComponentBreak(ItemStack aStack) {
-                return false;
-            }
-
-            @Override
-            public ITexture[] getTexture(IGregTechTileEntity baseMetaTileEntity, ForgeDirection side,
-                ForgeDirection facing, int colorIndex, boolean active, boolean redstoneLevel) {
-                return new ITexture[0];
-            }
-        };
+    @Override
+    public int survivalConstruct(ItemStack stackSize, int elementBudget, ISurvivalBuildEnvironment env) {
+        return survivialBuildPiece(
+            STRUCTURE_PIECE_MAIN,
+            stackSize,
+            HORIZONTAL_OFFSET,
+            VERTICAL_OFFSET,
+            DEPTH_OFFSET,
+            elementBudget,
+            env,
+            false,
+            true);
     }
 
     @Override
